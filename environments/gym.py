82673b7b239360353b7a10b1f1c4177e81cee038
Kolby <kolbytn@gmail.com>
2019-01-08 12:40:25 -0700

    hillclimb task

--- environments/gym.py
+++ environments/gym.py
@@ -29,3 +29,28 @@ class CartPoleEnvironment(RLEnvironment):
     def reset(self):
         """Returns observation (np.ndarray)"""
         return self._env.reset()
+
+
+class HillClimbEnvironmentFactory(EnvironmentFactory):
+    def __init__(self):
+        super(HillClimbEnvironmentFactory, self).__init__()
+
+    def new(self):
+        return HillClimbEnvironment()
+
+
+class HillClimbEnvironment(RLEnvironment):
+    def __init__(self):
+        super(HillClimbEnvironment, self).__init__()
+        self._env = gym.make('MountainCar-v0')
+
+    def step(self, action):
+        """action is type np.ndarray of shape [1] and type np.uint8.
+        Returns observation (np.ndarray), r (float), t (boolean)
+        """
+        s, r, t, _ = self._env.step(action.item())
+        return s, r, t
+
+    def reset(self):
+        """Returns observation (np.ndarray)"""
+        return self._env.reset()
---
+++ hillclimb.py
@@ -0,0 +1,29 @@
+from torch import nn
+
+import rl
+from environments.gym import HillClimbEnvironmentFactory
+import networks as nets
+
+
+def train_ppo():
+    factory = HillClimbEnvironmentFactory()
+    policy = nn.Sequential(
+        nets.FourLayerMlp(2, 2, hidden_dim=10),
+        nets.MultinomialNetwork()
+    )
+    value = nets.FourLayerMlp(2, 1, hidden_dim=10)
+
+    ppo = rl.PPO(factory, policy, value, experiment_name='hillclimb')
+    ppo.train(100, rollouts_per_epoch=100, max_episode_length=200, policy_epochs=5, batch_size=256)
+
+
+def train_dqn():
+    factory = HillClimbEnvironmentFactory()
+    action_value_network = nets.FourLayerMlp(2, 2, hidden_dim=10)
+
+    # dqn = rl.DQN(factory, action_value_network)
+    # dqn.train(100)
+
+
+if __name__ == '__main__':
+    train_ppo()
